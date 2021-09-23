b8f8b93db2a9473e727505b5ff7feee8078e55a7
balariant <91241863+balariant@users.noreply.github.com>
2021-09-23 10:53:52 +0300

    Create pspassword.ps1

---
+++ pspassword.ps1
@@ -0,0 +1,21 @@
+$letter='A','B' 
+
+$length=16
+
+$out=''
+
+For ($i=0; $i -lt $length; ++$i)
+{
+  $d=Get-Random -Minimum 0 -Maximum ($letter.Length-1)
+  
+  $out+=$letter[$d]
+
+  If((($i+1)%4 -eq 0) -and (($i+1) -ne $length))
+  {
+    $out+='-'
+  }
+}
+
+"Result: {0}" -f $out
+
+
